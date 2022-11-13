cd40d68fb937b2aaa0f90dfb3bee5bb7ed9fd3f4
messalih1 <mustaphaessalih1937@gmail.com>
2022-11-14 00:20:29 +0100

    vector: allocator member types + fixed push_back + some methods..

--- main.cpp
+++ main.cpp
@@ -6,31 +6,23 @@
 #include <iomanip>
 #include <string>
  
- #define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
-struct Buffer
-{
-int idx;
-char buff[BUFFER_SIZE];
-};
-#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+ 
 
 int main()
 {
+    
+     ft::vector<int> myvector;
 
-  ft::vector<int> myvector;
+    // myvector.push_back(3);
+  // set some content in the vector:
+  for (int i=0; i<100; i++) myvector.push_back(i);
 
-  // set some initial content:
-  for (int i=1;i<10;i++) myvector.push_back(i);
+  std::cout << "size: " << (int) myvector.size() << '\n';
+  std::cout << "capacity: " << (int) myvector.capacity() << '\n';
 
-  myvector.resize(5);
-  myvector.resize(8,100);
-  myvector.resize(12);
 
-  std::cout << "myvector contains:";
-  for (int i=0;i<myvector.size();i++)
-    std::cout << ' ' << myvector[i];
-  std::cout << '\n';
-  
+    
+
+
 
 }
--- vector.hpp
+++ vector.hpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <string>
 #include <exception>
-#include <algorithm>    // std::copy
+#include <algorithm>     
 
 
 using std::string;
@@ -19,285 +19,130 @@ using std::stack;
 using std::map;
 
 
-
 namespace ft
 {
-
-    template<typename T>
+    template < typename T, typename A = std::allocator<T>>
     class vector
     {
         private:
             T * block;
             T * block_copy;
-            int vector_size;
-            int vector_capacity;
+            size_t vector_size;
+            size_t vector_capacity;
+            A _alloc;
             T tmp;
-            std::allocator<T> alloc;
-            // std::allocator<T> alloc;
+            
+            
         public:
-            vector();
-            vector(int n);
-            vector(int n, const T & val);
-            vector& operator= (const vector& x);
-            T & operator[](int index);
-            unsigned int size()const;
-            unsigned int capacity()const;
-            void push_back(const T & val);
-            void push_back(const T * val);
-            bool empty() const;
-            void resize(int n);
-            void resize(int n, const T & val);
-            void resize(int n, const T * val);
-            void reserve (int n);
-            ~vector();
-    };
-    class length_error : public std::exception{
-        public:
-            const char * what () const throw()
-            {
-                return "length error";
-            }
-    };
-    class logic_error : public std::exception{
-        public:
-            const char * what () const throw()
-            {
-                return "length error";
-            }
-    };
-}
-
-template<typename T>
-    
-ft::vector<T>::vector()
-{
-    block = alloc.allocate(0);
-    // block_copy = alloc.allocate(0);
-    vector_capacity = 0;
-    vector_size = 0;
-}
-
-template<typename T>
-    
-ft::vector<T>::vector(int n)
-{
-    if(n < 0 || n > 2147483647)// check the max size
-        throw length_error();
-   
-    block = alloc.allocate(n );
-   
-    // block_copy = alloc.allocate(n );
-    
-    vector_capacity = n;
-    vector_size = n;
-
-   
-}
-
-template<typename T>
-    
-ft::vector<T>::vector(int n, const T & val)
-{
-    if(n < 0 || n > 2147483647)
-        throw length_error();
-    vector_capacity  = n;
-    vector_size = n;
-    block = alloc.allocate(n);
-    // block_copy = alloc.allocate(n);
-    while(n--)
-        block[n] = val;
-}
-
-template<typename T>
-    
-ft::vector<T>& ft::vector<T>::operator=(const ft::vector<T>&x)
-{
-    if(this != &x)
-    {
-        vector_capacity = x.vector_capacity;
-        vector_size = x.vector_size;
-        int tmp = vector_capacity;
-        while (tmp--)
-            block[tmp] = x.block[tmp];
-    }
-    return *this;
-}
-
-template<typename T>
-
-T & ft::vector<T>::operator [] (int index)
-{
-    if(index < 0 ) // ||  index >=  size of vector
-        return tmp;
-    return block[index];
-}
- 
- 
-template<typename T>
-
-void ft::vector<T>::push_back(const T * val)
-{
-    if(!val)
-        throw logic_error();
-}
-template<typename T>
-
-void ft::vector<T>::reserve(int n)
-{   
-    if(n < 0 || n > 2147483647)
-        throw length_error();
-    if(n > capacity())// 1
-    {
-        block_copy = alloc.allocate(size());
         
-        for (unsigned int i = 0; i < size(); i++)
+        typedef  A allocator_type;
+        typedef typename allocator_type::value_type      value_type;// the data entred by user
+
+        typedef typename allocator_type::size_type       size_type;
+        typedef typename allocator_type::difference_type difference_type;
+
+        typedef typename allocator_type::pointer         pointer;
+        typedef typename allocator_type::const_pointer   const_pointer;
+
+        typedef typename allocator_type::reference       reference;
+        typedef typename allocator_type::const_reference const_reference;
+
+        typedef typename allocator_type::pointer         iterator;
+        typedef typename allocator_type::const_pointer   const_iterator;
+
+        
+        // if pass alloactor call the allocator if not passed an allocetor will enter to default constructor is optonal
+        vector(const allocator_type& alloc = allocator_type())// by this syntax we call a default constructor of another class 
         {
-            block_copy[i] = std::move(block[i]);
+            vector_size = 0;
+            vector_capacity = 0;
+            _alloc = alloc;
         }
-        // alloc.deallocate(block,size());
-        block = alloc.allocate(n);
-        std::swap(block,block_copy);
-        alloc.deallocate(block_copy,size());
-    }
-    vector_size +=1;
-    vector_capacity = n;
-}
-
-template<typename T>
-
-unsigned int ft::vector<T>::size()const
-{
-    return vector_size;
-}
-template<typename T>
-
-unsigned int ft::vector<T>::capacity()const
-{
-    return vector_capacity;
-}
- 
-template<typename T>
-
-void ft::vector<T>::push_back(const T & val)
-{
-    if(empty())
-    {
-        block = alloc.allocate(1);
-        block[vector_size] = val;
-        vector_size++; 
-        vector_capacity++; 
-    }
-    else if(vector_size < vector_capacity)
-    {
-        block[vector_size] = val;
-        vector_size++;
-    }
-    else if (vector_size == capacity())
-    {
-        vector_capacity = vector_capacity * 2;//2
-        block_copy = alloc.allocate(size());
-        for (unsigned int i = 0; i < size(); i++)
+        vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
         {
-            block_copy[i] = std::move(block[i]);
+            // if no value entered wil return 0 val = 0
+            vector_size = n;
+            vector_capacity = n;
+            _alloc = alloc;
+            block = _alloc.allocate(n);
+            _alloc.construct(block,val);
         }
-        alloc.deallocate(block,size());
-        block = alloc.allocate(capacity());
-        for (unsigned int i = 0; i < size(); i++)
+    // template <class InputIterator>         vector (InputIterator first, InputIterator last,                 const allocator_type& alloc = allocator_type());
+        vector (const vector& x)
         {
-            block[i] = std::move(block_copy[i]);
+            vector_size = x.vector_size;
+            vector_capacity = x.vector_capacity;
+            _alloc = x._alloc;
+            for (size_type i = 0; i < vector_size; i++)
+                this->block[i] = x.block[i];
         }
-        alloc.deallocate(block_copy,size());
-        block[vector_size] = val;
-        vector_size++; 
-    }
-    
-    
+        vector& operator= (const vector& x)
+        {
+            if(this != &x)
+            {
+                vector_size = x.vector_size;
+                vector_capacity = x.vector_capacity;
+                _alloc = x._alloc;
+                for (size_type i = 0; i < vector_size; i++)
+                    this->block[i] = x.block[i];
+            }
+            return *this;
+
+        }
+        size_type size() const
+        {
+            return vector_size;
+        }
+        size_type capacity() const
+        {
+            return vector_capacity;
+        }
+        bool empty()const
+        {
+            return (vector_size == 0) ? true : false; 
+        }
+        void push_back (const value_type& val)
+        {
+            if(empty())
+            {
+                block = _alloc.allocate(1);
+                block[vector_size] = val;
+                vector_size++; 
+                vector_capacity++; 
+            }
+            else if(vector_size < vector_capacity)
+            {
+                block[vector_size] = val;
+                vector_size++;
+            }
+            else if (vector_size == capacity())
+            {
+                vector_capacity = vector_capacity * 2;//2
+                block_copy = _alloc.allocate(size());
+                for (size_type i = 0; i < size(); i++)
+                {
+                    block_copy[i] = std::move(block[i]);
+                }
+                _alloc.deallocate(block,size());
+                block = _alloc.allocate(capacity());
+                for (size_type i = 0; i < size(); i++)
+                {
+                    block[i] = std::move(block_copy[i]);
+                }
+                _alloc.deallocate(block_copy,size());
+                block[vector_size] = val;
+                vector_size++; 
+            }
+        }
+        ~vector()
+        {
+
+        }
+    };
 }
+
+    
  
-template<typename T>
-
-bool ft::vector<T>::empty()const
-{
-    return (vector_size == 0) ? true : false; 
-}
- 
-
-template<typename T>
-
-void ft::vector<T>::resize(int n) 
-{
-    if(n < 0 || n > 2147483647)
-        throw length_error();
-
-    block_copy = alloc.allocate(size());
-    for (unsigned int i = 0; i < size(); i++)
-    {
-        block_copy[i] = std::move(block[i]);
-    }
-    alloc.deallocate(block,size());
-    block = alloc.allocate(n);
-    for (unsigned int i = 0; i < n; i++)
-    {
-        block[i] = std::move(block_copy[i]);
-    }
-    alloc.deallocate(block_copy,size());
-    vector_size = n;
-    vector_capacity = n;
-}
- 
-
-template<typename T>
-
-void ft::vector<T>::resize(int n,const T * val)
-{
-    if(!val)
-        throw logic_error();
-} 
-
-// fix erroe in resize
-template<typename T>
-
-void ft::vector<T>::resize(int n,const T & val)
-{
-    int  i;
-
-    if(n < 0 || n > 2147483647)
-        throw length_error();
-    
-    block_copy = alloc.allocate(size());
-    
-    i = 0;
-    while(i < size())
-    {
-        block_copy[i] = std::move(block[i]);
-        i++;
-    }
-    alloc.deallocate(block,size());
-    block = alloc.allocate(n);
-    i = 0;
-    
-    while(i < size())
-    {
-        block[i] = std::move(block_copy[i]);
-        i++;
-    }
-    while(i < n)
-    {
-        block[i] = val;
-        i++;
-    }
-    alloc.deallocate(block_copy,size());
-    vector_size = n;
-    vector_capacity = n;
-} 
-
-template<typename T>
-
-ft::vector<T>::~vector()
-{
-    alloc.deallocate(block,vector_size);
-}
-
 
 
 #endif
